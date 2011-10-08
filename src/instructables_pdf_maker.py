9a42e76a53e3140e759e9fa41ce1b3182ac19f5d
Yigit Ozkan <yigitozkan2804@gmail.com>
2011-10-08 22:32:05 +0200

    getting one hell of a lot even with git :D

--- src/instructables_pdf_maker.py
+++ src/instructables_pdf_maker.py
@@ -10,9 +10,17 @@ Created on Sun Sep 11 20:34:45 2011
 @licence: GNU GPL v3+
 @author: Yigit Ozkan < yigitozkan2804@gmail.com >
 """
+def get_html(url): #get the html source of a body
+    response = urllib2.urlopen(url)
+    html = response.read()
+    return html
 
-
+def find_all_links(html): #look up all links in a html source
+    linklist = re.findall('<a href="(.*?)">',html)
+    return linklist
+    
 def main():
+    
     
 
 if __name__ == '__main__':
